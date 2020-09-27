43fbe0919470828e9d14a545b87e13c34967f4f4
sanxCode <70911574+sanxCode@users.noreply.github.com>
2020-09-27 22:58:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+console.log('Test')
+
+// IF User adds a note add it to the local storage
+
+let addBtn = document.getElementById('addbtn');
+addBtn.addEventListener('click', (e) => {
+    let addTxt = document.getElementById('addtxt');
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    addTxt.value = '';
+    console.log(notesObj);
+
+    showNotes();
+})
+showNotes();
+// Function to show elements form local storage
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    let html = ""
+    notesObj.forEach((element, index) => {
+        html += `<div class="noteCard card mx-2 my-2 " style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <p class="card-text">${element}</p>
+                        <button id='${index}' onclick='deleteNote(this.id)' class="btn btn-primary">Delete Note</button>
+                    </div>
+                </div>`
+    });
+
+    let notesElm = document.getElementById('notes');
+    if (notes.length != 0) {
+        notesElm.innerHTML = html;
+    }
+    else{
+        notesElm.innerHTML = 'Nothing to show! Use "Add a Note" section above to add a notes'
+    }
+}
+
+// Function to delete a note
+function deleteNote(index) {
+
+    console.log('i am deleteing', index);
+    let notes = localStorage.getItem("notes");
+
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    showNotes();
+}
+
+let search = document.getElementById('searchTxt');
+search.addEventListener('input',function(){
+    let inputVal = search.value.toLowerCase();
+    // console.log('input event fire', inputVal);
+
+    let noteCards = document.getElementsByClassName('noteCard');
+
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt = element.getElementsByTagName('p')[0].innerText;
+        
+        // console.log(cardTxt)
+        if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+    })
+})
+
+// further features
+// 1. title
+// 2.mark a note as important 
+// 3. Seprate note by user 
